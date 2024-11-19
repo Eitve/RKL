df6f748573cbade5ae3ee1b3663efcd399635dbc
Eitve <eitve59@gmail.com>
2024-11-19 18:14:21 +0100

    Upgraded to Expo SDK52, fixed dependancy issues beccause of other import version differences

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './index';  // Assuming `index.tsx` is your home screen file
+
+import HomeScreen from './index'; // Assuming `index.tsx` is your home screen file
 import StandingsScreen from './standings';
 import NewsDetailScreen from './NewsDetailScreen';
 import ScheduleScreen from './schedule';
 import StatisticsScreen from './statistics';
 import { ThemeProvider, createTheme } from '@rneui/themed';
 
+// Bottom Tab Navigator and Stack Navigator
 const Tab = createBottomTabNavigator();
 const HomeStack = createNativeStackNavigator();
 
+// Define a custom theme using RNE's `createTheme`
 const theme = createTheme({
   lightColors: {
     primary: '#6200ea',
@@ -20,6 +24,7 @@ const theme = createTheme({
   },
 });
 
+// Stack Navigator for Home and NewsDetail screens
 function HomeStackScreen() {
   return (
     <HomeStack.Navigator>
@@ -37,15 +42,34 @@ function HomeStackScreen() {
   );
 }
 
+// Main App Component with Bottom Tab Navigator
 export default function AppTabs() {
   return (
-    <ThemeProvider theme={theme}>
-      <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name="Schedule" component={ScheduleScreen} options={{ headerShown: false }} />
-        <Tab.Screen name="Standings" component={StandingsScreen} options={{ headerShown: false }} />
-        <Tab.Screen name="Home" component={HomeStackScreen} options={{ headerShown: false }} />
-        <Tab.Screen name="Statistics" component={StatisticsScreen} options={{ headerShown: false }} />
-      </Tab.Navigator>
-    </ThemeProvider>
+    <NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <Tab.Navigator initialRouteName="Home">
+          <Tab.Screen
+            name="Schedule"
+            component={ScheduleScreen}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Standings"
+            component={StandingsScreen}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Home"
+            component={HomeStackScreen}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Statistics"
+            component={StatisticsScreen}
+            options={{ headerShown: false }}
+          />
+        </Tab.Navigator>
+      </ThemeProvider>
+    </NavigationContainer>
   );
 }
